715d0c9da1649160349855b895c9b01c9bd7f4cf
LorenzoFasolino <fasolinolorenzo@gmail.com>
2019-12-29 16:20:26 +0100

    Aggiunto OrariDocControl

---
+++ SmartBooking/src/gestioneOrari/OrariDocControl.java
@@ -0,0 +1,28 @@
+package gestioneOrari;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import Model.DisponibilitaModel;
+
+public class OrariDocControl {
+	
+	public OrariDocControl(
+			
+			){}
+	
+	public void aggiungiFirstOrario(String mDocente,ArrayList<String> giorni,ArrayList<String> orariInizio, ArrayList<String> orariFine) throws SQLException{
+		
+		DisponibilitaModel  disponibilitaModel = new DisponibilitaModel();
+		
+		int i = 0;
+		
+		for(i= 0; i<giorni.size();i++){
+			
+			disponibilitaModel.aggiungiOrario(mDocente, giorni.get(i), orariInizio.get(i), orariFine.get(i));
+			
+		}
+		
+	}
+
+}
