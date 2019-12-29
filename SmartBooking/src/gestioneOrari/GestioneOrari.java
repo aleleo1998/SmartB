ebd94d5daae590af98bb9b610bc56208d55cae14
ozne2 <ozne23@outlook.it>
2019-12-29 16:10:01 +0100

    inizio gestion orari

--- SmartBooking/src/gestioneOrari/GestioneOrari.java
+++ SmartBooking/src/gestioneOrari/GestioneOrari.java
@@ -3,10 +3,17 @@
  */
 package gestioneOrari;
 
+import java.util.ArrayList;
+
+import Model.Docente;
+
 /**
  * @author ozne2
  *
  */
 public interface GestioneOrari {
+	
+	public void aggiungiFirstOrario(Docente d,ArrayList<String> giorni,ArrayList<String> orariInizio, ArrayList<String> orariFine);
+	
 
 }
--- SmartBooking/src/gestioneOrari/GestioneOrariConcrete.java
+++ SmartBooking/src/gestioneOrari/GestioneOrariConcrete.java
@@ -3,12 +3,31 @@
  */
 package gestioneOrari;
 
+import java.util.ArrayList;
+
+import Model.DisponibilitaModel;
+import Model.Docente;
+
 /**
  * @author ozne2
  *
  */
 public class GestioneOrariConcrete implements GestioneOrari {
 	
+	public void aggiungiFirstOrario(Docente d,ArrayList<String> giorni,ArrayList<String> orariInizio, ArrayList<String> orariFine) {
+		
+		
+		DisponibilitaModel model = new DisponibilitaModel();
+		
+		for(String g :  giorni) {
+			
+			
+		}
+		
+		
+		
+		
+	}
 	
 
 }
