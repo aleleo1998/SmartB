660bc54bd9a9876bf6c60753d87d3dc3e3992570
ozne2 <ozne23@outlook.it>
2020-01-13 13:37:21 +0100

    testing dispo

--- SmartBooking/src/Testing/DisponibilitaTest.java
+++ SmartBooking/src/Testing/DisponibilitaTest.java
@@ -2,7 +2,6 @@ package Testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.sql.Time;
 import org.junit.jupiter.api.Test;
 
 import Model.Disponibilita;
@@ -18,28 +17,28 @@ class DisponibilitaTest {
 
 	  @Test
 	  void testGetGiorno() {
-		Time t = new Time(0);
-	    Disponibilita r = new Disponibilita("Lunedi", t, "0512100001");
+		String t = "10:00";
+		Disponibilita r = new Disponibilita("Lunedi", t, "0512100001");
 	    assertEquals("Lunedi", r.getGiorno());
 	  }
 	  
 	  @Test
 	  void testGetOra() {
-		Time t = new Time(0);
-	    Disponibilita r = new Disponibilita("Lunedi", t, "0512100001");
+		  String t = "10:00";
+		  Disponibilita r = new Disponibilita("Lunedi", t, "0512100001");
 	    assertEquals(t, r.getOra());
 	  }
 	  
 	  @Test
 	  void testGetMatricolaDocente() {
-		Time t = new Time(0);
+		  String t = "10:00";
 	    Disponibilita r = new Disponibilita("Lunedi", t, "0512100001");
 	    assertEquals("0512100001", r.getMatricolaDocente());
 	  }
 	  
 	  @Test
 	  void testSetGiorno() {
-		Time t = new Time(0);
+		  String t = "10:00";
 	    Disponibilita r = new Disponibilita("", t, "0512100001");
 	    r.setGiorno("Lunedi");
 	    assertEquals("Lunedi", r.getGiorno());
@@ -47,7 +46,7 @@ class DisponibilitaTest {
 	  
 	  @Test
 	  void testSetOra() {
-		Time t = new Time(0);
+		String t = "10:00";
 	    Disponibilita r = new Disponibilita("Lunedi", null, "0512100001");
 	    r.setOra(t);
 	    assertEquals(t, r.getOra());
@@ -55,7 +54,7 @@ class DisponibilitaTest {
 	  
 	  @Test
 	  void testSetMatricolaDocente() {
-		Time t = new Time(0);
+		String t = "10:00";
 	    Disponibilita r = new Disponibilita("Lunedi", t, "");
 	    r.setMatricolaDocente("0512100001");
 	    assertEquals("0512100001", r.getMatricolaDocente());
