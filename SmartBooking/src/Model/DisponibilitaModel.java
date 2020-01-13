b28cac24b7b34748769bea02deb752237066ff4e
LorenzoFasolino <fasolinolorenzo@gmail.com>
2020-01-13 13:25:22 +0100

    PROVA

--- SmartBooking/src/Model/DisponibilitaModel.java
+++ SmartBooking/src/Model/DisponibilitaModel.java
@@ -19,7 +19,7 @@ import DBConnection.DriverManagerConnectionPool;
  */
 public class DisponibilitaModel {
 	
-private static final String TABLE_NAME = "Disponibilit‡†";
+private static final String TABLE_NAME = "Disponibilit√†";
 	
 	/**
 	 * @param Disponibilita
